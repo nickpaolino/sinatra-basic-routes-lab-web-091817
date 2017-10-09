396788503d823d7855dab43653210db9c72a24ac
Nick Paolino <github email address>
2017-10-08 20:31:05 -0400

    Finished

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Nick"
+  end
+
+  get '/hometown' do
+    "My hometown is Providence, RI"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is the Koln Concert"
+  end
 end
